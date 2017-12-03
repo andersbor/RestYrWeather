c4af64704c15a70117490e316c50f37b5af49c03
andersbor <anbo@easj.dk>
2017-12-03 19:02:57 +0100

    refactored

--- RESTyr/Program.cs
+++ RESTyr/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Xml;
@@ -8,66 +9,74 @@ namespace RESTyr
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string Uri = "http://www.yr.no/sted/Danmark/Sjælland/Roskilde/varsel.xml";
+
+        static void Main()
         {
             using (HttpClient client = new HttpClient())
             {
-                string uri = "http://www.yr.no/sted/Danmark/Sjælland/Roskilde/varsel.xml";
-                string xmlString = GetProductAsync(client, uri).Result;
-                Console.WriteLine(xmlString);
-
-                XmlDocument xmlDocument = new XmlDocument();
-                xmlDocument.LoadXml(xmlString);
-                XmlNodeList namesXmlList = xmlDocument.GetElementsByTagName("time");
-                List<Weather> weather = new List<Weather>();
-                for (int i = 0; i < namesXmlList.Count; i++)
+                HttpResponseMessage response = client.GetAsync(Uri).Result;
+                
+                if (response.IsSuccessStatusCode)
                 {
-                    XmlNode namesNode = namesXmlList[i];
-                    string from = namesNode.Attributes["from"].Value;
-                    string to = namesNode.Attributes["to"].Value;
-
-                    XmlNode temperatureNode = namesNode.SelectSingleNode("temperature");
-                    string temperatureString = temperatureNode.Attributes["value"].Value;
-                    int temperature = int.Parse(temperatureString);
-
-                    XmlNode pressureNode = namesNode.SelectSingleNode("pressure");
-                    string pressureSTring = pressureNode.Attributes["value"].Value;
-                    double pressure = double.Parse(pressureSTring);
-
-                    XmlNode precipitationNode = namesNode.SelectSingleNode("precipitation");
-                    string precipitationString = precipitationNode.Attributes["value"].Value;
-                    double precipitation = double.Parse(precipitationString);
-                    //string countryName = namesXmlList[i].InnerText;
-         
-                    Weather w = new Weather { From = from, To = to, Temperature = temperature, Pressure = pressure, Precipitation = precipitation };
-                    weather.Add(w);
+                    string xmlString = GetProductAsync(client, Uri).Result;
+                    Console.WriteLine(xmlString);
+                    IEnumerable<Weather> weather = GetWeatherFromXml(xmlString);
+                    Console.WriteLine(string.Join("\n", weather));
                 }
-                Console.WriteLine(string.Join("\n", weather));
+                else
+                {
+                    Console.WriteLine("HttpResponse status code:" + (int)response.StatusCode + " " + response.ReasonPhrase);
+                }
             }
         }
 
-        static async Task<string> GetProductAsync(HttpClient client, string path)
+        private static async Task<string> GetProductAsync(HttpClient client, string path)
         {
             HttpResponseMessage response = await client.GetAsync(path);
             if (response.IsSuccessStatusCode)
             {
                 return await response.Content.ReadAsStringAsync();
             }
-            return null;
+            throw new IOException("HttpResponse " + response.StatusCode + " " + response.ReasonPhrase);
         }
-    }
 
-    class Weather
-    {
-        public string From { get; set; }
-        public string To { get; set; }
-        public int Temperature { get; set; }
-        public double Precipitation { get; set; }
-        public double Pressure { get; set; }
-
-        public override string ToString()
+        private static IEnumerable<Weather> GetWeatherFromXml(string xmlString)
         {
-            return From + " " + To + " " + Temperature + " " + Pressure + " " + Precipitation;
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xmlString);
+
+            XmlNodeList namesXmlList = xmlDocument.GetElementsByTagName("time");
+            IList<Weather> weather = new List<Weather>();
+            for (int i = 0; i < namesXmlList.Count; i++)
+            {
+                XmlNode namesNode = namesXmlList[i];
+                string from = namesNode.Attributes["from"].Value;
+                string to = namesNode.Attributes["to"].Value;
+
+                XmlNode temperatureNode = namesNode.SelectSingleNode("temperature");
+                string temperatureString = temperatureNode.Attributes["value"].Value;
+                int temperature = int.Parse(temperatureString);
+
+                XmlNode pressureNode = namesNode.SelectSingleNode("pressure");
+                string pressureSTring = pressureNode.Attributes["value"].Value;
+                double pressure = double.Parse(pressureSTring);
+
+                XmlNode precipitationNode = namesNode.SelectSingleNode("precipitation");
+                string precipitationString = precipitationNode.Attributes["value"].Value;
+                double precipitation = double.Parse(precipitationString);
+
+                Weather w = new Weather
+                {
+                    From = from,
+                    To = to,
+                    Temperature = temperature,
+                    Pressure = pressure,
+                    Precipitation = precipitation
+                };
+                weather.Add(w);
+            }
+            return weather;
         }
     }
 }
---
+++ RESTyr/Weather.cs
@@ -0,0 +1,16 @@
+﻿namespace RESTyr
+{
+    public class Weather
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Temperature { get; set; }
+        public double Precipitation { get; set; }
+        public double Pressure { get; set; }
+
+        public override string ToString()
+        {
+            return From + " " + To + " " + Temperature + " " + Pressure + " " + Precipitation;
+        }
+    }
+}
